a96071ff3339506f3762527f4f4d97e1567ad30b
HFTlab <arbitragecryptobotnet@gmail.com>
2023-07-19 12:49:22 +0200

    added exception for slippage on apollox

--- multi_bot.py
+++ multi_bot.py
@@ -493,16 +493,23 @@ class MultiBot:
         while True:
             ob_sell = client_sell.get_orderbook()[client_sell.symbol]
             ob_buy = client_buy.get_orderbook()[client_buy.symbol]
+            if client_sell.EXCHANGE_NAME == 'APOLLOX':
+                ob_sell_time_shift = 5 * self.deal_pause * 1000
+            else:
+                ob_sell_time_shift = self.deal_pause * 1000
+            if client_buy.EXCHANGE_NAME == 'APOLLOX':
+                ob_buy_time_shift = 5 * self.deal_pause * 1000
+            else:
+                ob_buy_time_shift = self.deal_pause * 1000
             current_timestamp = int(time.time() * 1000)
-            deal_pause_ms = self.deal_pause * 1000
-            if current_timestamp - ob_sell['timestamp'] > 3 * deal_pause_ms:
+            if current_timestamp - ob_sell['timestamp'] > ob_sell_time_shift:
                 if self.state == BotState.BOT:
                     self.state = BotState.SLIPPAGE
                     self.ob_alert_send(client_sell, client_buy, ob_sell['timestamp'])
                     client_slippage = client_sell
                 time.sleep(5)
                 continue
-            elif current_timestamp - ob_buy['timestamp'] > 3 * deal_pause_ms:
+            elif current_timestamp - ob_buy['timestamp'] > ob_buy_time_shift:
                 if self.state == BotState.BOT:
                     self.state = BotState.SLIPPAGE
                     self.ob_alert_send(client_buy, client_sell, ob_buy['timestamp'])
@@ -512,12 +519,7 @@ class MultiBot:
             elif ob_sell['asks'] and ob_sell['bids'] and ob_buy['asks'] and ob_buy['bids']:
                 if self.state == BotState.SLIPPAGE:
                     self.state = BotState.BOT
-                    try:
-                        self.ob_alert_send(client_sell, client_buy, ob_sell['timestamp'], client_slippage)
-                    except Exception:
-                        print("\n" * 10)
-                        traceback.print_exc()
-                        print("\n" * 10)
+                    self.ob_alert_send(client_sell, client_buy, ob_sell['timestamp'], client_slippage)
                     client_slippage = None
                 return ob_sell, ob_buy
 
