40665c41233091bc62411cbcd05f63b1e8e1a042
HFTlab <arbitragecryptobotnet@gmail.com>
2023-08-14 08:54:01 +0200

    added infrastructure for running multibot without docker

---
+++ run_multibot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python3 multi_bot.py ./configs/eth_config_binance_apollox.ini
+python3 multi_bot.py ./configs/eth_config_dydx_apollox.ini
+python3 multi_bot.py ./configs/eth_config_dydx_binance.ini
+python3 multi_bot.py ./configs/eth_config_kraken_apollox.ini
+python3 multi_bot.py ./configs/eth_config_kraken_binancex.ini
+python3 multi_bot.py ./configs/eth_config_dydx_kraken.ini
