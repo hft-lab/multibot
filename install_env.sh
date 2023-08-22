19b6a0a20db430c4f9cdb86fb71494b970520111
HFTlab <arbitragecryptobotnet@gmail.com>
2023-08-22 17:47:00 +0200

    added & wait to bin file install_env

--- install_env.sh
+++ install_env.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt upgrade -y
-sudo apt install python3-pip
-sudo pip3 install --upgrade pip3
-sudo pip3 install dydx_v3_python
-sudo pip3 install python-dotenv
-sudo pip3 install --no-cache-dir --user -r requirements.txt
-sudo pip3 install --upgrade requests
-touch rates.txt
+sudo apt update & wait
+sudo apt upgrade -y & wait
+sudo apt install python3-pip & wait
+sudo pip3 install --upgrade pip3 & wait
+sudo pip3 install dydx_v3_python & wait
+sudo pip3 install python-dotenv & wait
+sudo pip3 install --no-cache-dir --user -r requirements.txt & wait
+sudo pip3 install --upgrade requests & wait
