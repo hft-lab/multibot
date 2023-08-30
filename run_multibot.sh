646b8807baba28ddcd7f1346a5b3d3ed05a6a725
HFTlab <arbitragecryptobotnet@gmail.com>
2023-08-30 15:24:05 +0200

    updated single config and run bin file

--- run_multibot.sh
+++ run_multibot.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-python3 multi_bot.py ./configs/eth_config_binance_apollox.ini &
-python3 multi_bot.py ./configs/eth_config_dydx_apollox.ini &
-python3 multi_bot.py ./configs/eth_config_dydx_binance.ini &
-python3 multi_bot.py ./configs/eth_config_kraken_apollox.ini &
-python3 multi_bot.py ./configs/eth_config_kraken_binance.ini &
-python3 multi_bot.py ./configs/eth_config_dydx_kraken.ini &
+python3 multi_bot.py config.ini
