3548f729e36eb0d72d6969acbcecf1b3f2ae1451
HFTlab <arbitragecryptobotnet@gmail.com>
2023-12-10 15:15:17 +0100

    fixed logging mode

--- multi_bot.py
+++ multi_bot.py
@@ -28,7 +28,7 @@ dictConfig({'version': 1, 'disable_existing_loggers': False, 'formatters': {
     'simple': {'format': '[%(asctime)s][%(threadName)s] %(funcName)s: %(message)s'}},
             'handlers': {'console': {'class': 'logging.StreamHandler', 'level': 'DEBUG', 'formatter': 'simple',
                                      'stream': 'ext://sys.stdout'}},
-            'loggers': {'': {'handlers': ['console'], 'level': 'DEBUG', 'propagate': False}}})
+            'loggers': {'': {'handlers': ['console'], 'level': 'INFO', 'propagate': False}}})
 logger = logging.getLogger(__name__)
 
 leverage = float(config['SETTINGS']['LEVERAGE'])
